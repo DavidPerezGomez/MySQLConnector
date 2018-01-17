e827ede7f999bb8d778f5cd6216c967de0d370bb
DavidPerezGomez <davidperezgo@gmail.com>
2018-01-17 19:58:23 +0100

    changed test

--- test.py
+++ test.py
@@ -1,21 +1,11 @@
 from lib.connector import MysqlConnector
 
 c = MysqlConnector()
-c.connect()
-c.close()
-
 c.set_username('faeria')
-c.connect()
-c.close()
-
 c.set_password('faeria')
-c.connect()
-c.close()
-
 c.set_host('localhost')
-c.connect()
-c.close()
-
 c.set_database('Faeria')
 c.connect()
+sql = ";insert into deck values ('deck9'); SELECT * from deck;"
+c.exec(sql)
 c.close()
