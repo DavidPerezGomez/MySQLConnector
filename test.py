e8a7324a527247968073a949b8ea8c62f040da1b
DavidPerezGomez <davidperezgo@gmail.com>
2018-01-18 22:14:53 +0100

    changed test

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ c.set_password('faeria')
 c.set_host('localhost')
 c.set_database('Faeria')
 c.connect()
-sql = ";insert into deck values ('deck9'); SELECT * from deck;"
-c.exec(sql)
+sql = ";Select * from card; INSERT INTO deck VALUES ('deck10'); SELECT * from deck;"
+print(c.exec(sql))
 c.close()
