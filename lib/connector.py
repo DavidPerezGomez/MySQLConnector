09a483a20eaddc8ccac68bb7e4d3d1fe279eb452
DavidPerezGomez <davidperezgo@gmail.com>
2018-01-18 22:14:29 +0100

    added separate functions to execute multiple sql statements or a seungle one. documented code

--- lib/connector.py
+++ lib/connector.py
@@ -3,12 +3,11 @@ import mysql.connector
 
 class MysqlConnector:
 
-    _username = 'faeria'
-    _password = 'faeria'
-    _host = 'localhost'
-    _database = 'Faeria'
+    _username = None
+    _password = None
+    _host = None
+    _database = None
     _connected = False
-    # TODO mover info de conexión a config.py o algo asi...
     _conn = None
     _cursor = None
 
@@ -19,27 +18,35 @@ class MysqlConnector:
         self.set_database(database)
 
     def is_connected(self):
+        '''Returns True if connection with a database has been established, False otherwise.'''
         return self._connected
 
     def get_connected_database(self):
+        '''Returns the name of the database connected to, False if it's not connected to any database.'''
         if self.is_connected():
             return self._conn.database
         else:
             return False
 
     def set_username(self, username):
+        '''Sets the username to be connected with'''
         self._username = username
 
     def set_password(self, password):
+        '''Sets the password to be connected with'''
         self._password = password
 
     def set_host(self, host):
+        '''Sets the host to be connected with'''
         self._host = host
 
     def set_database(self, database):
+        '''Sets the username to be connected to'''
         self._database = database
 
     def connect(self):
+        '''Connects to the database with the username, host and password given.
+        Returns True if successful, False otherwise'''
         if not self.is_connected():
             error = False
             if self._username is None:
@@ -63,30 +70,76 @@ class MysqlConnector:
                 except mysql.connector.Error as e:
                     print('Connection could not be established.')
                     print(e)
-            else:
-                self.close()
         else:
             print('Already connected to a database. Please close current connection before opening a new one.')
+        return self.is_connected()
 
     def close(self):
+        '''Closes the connection with the database'''
         if self.is_connected():
             tmp = self._conn.database
             self._conn.close()
             self._connected = False
             print('Closed connection with database {}.'.format(tmp))
         else:
-            print('Connection was not established.')
+            print('Connection had not been established.')
 
     def exec(self, operation=''):
+        '''Executes the operation given. Only one operations is accepted.
+        If multiple statements are passed, only the first one will be executed.
+        Returns a list where each of its elements is one row of the view
+        resulting from the sql statement.
+        Each one of those row comes in the form of a tuple, with every element
+        coresponding to a comlumn.
+        In the case of statements where no rows are returned
+        (INSERT, UPDATE, DELETE, etc.), it will instead return the number of
+        rows affected by the operation.'''
+        # TODO mencionar lo de que si hay ';' en el propio sql se jode todo
+        # TODO testear con nullvalues, etc.
         if self.is_connected():
-            operations = self._parse_operation(operation)
+            operations = self._parse_operations(operation)
+            results = self._execute([operations[0]])
+            return results[0]
+        else:
+            print('Not connected to any database.')
+
+    def exec_mult(self, operation=''):
+        '''Executes the operation or operations given. Multiple operations can
+        be executed on a single call. For that, all the statements must come on
+        a single string and be separated by ';'. A ';' at the end is not
+        required when a single statement is passed.
+        Returns a list where each element is the result of one of the sql
+        statements.
+        Each of the results is a list itself and each of its elements is one
+        row of the view resulting from the sql statement.
+        Each one of those row comes in the form of a tuple, with every element
+        coresponding to a comlumn.
+        In the case of statements where no rows are returned
+        (INSERT, UPDATE, DELETE, etc.), the list corresponding to the
+        result of said statement will instead be the number of rows affected by
+        the operation.
+        ie.:
+        Tables:
+        POKEMON(name, type1, type2)
+        USER(number, name, age)
+        Statement:
+        SELECT * FROM pokemon; INSERT INTO user VALUES (4, manuel, 45); SELECT name FROM user;
+        Result:
+        [[(pikachu, electric, NULL), (bisharp, dark, steel), (castform, normal, NULL)],
+        1,
+        [(jose,), (pedro,), (marcos,), (manuel,)]]
+        '''
+        if self.is_connected():
+            operations = self._parse_operations(operation)
             results = self._execute(operations)
             return results
         else:
             print('Not connected to any database.')
 
     @staticmethod
-    def _parse_operation(operation):
+    def _parse_operations(operation):
+        '''Parses a string containing multiple sql statements separated by ';'
+        and returns a list contaning each of those statements'''
         if operation.startswith(';'):
             operation = operation[1:]
         if operation.endswith(';'):
