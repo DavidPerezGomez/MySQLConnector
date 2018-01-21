59993de972ef6ddc3979286fed97e028b34ac453
DavidPerezGomez <davidperezgo@gmail.com>
2018-01-21 23:19:45 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='mysqlconnector',
+      version='1.0',
+      description='',
+      author='',
+      author_email='',
+      url='',
+      py_modules=['mysqlconnector/connector'],
+     )
